23bcd8e83094c746f2ffe01a52b50f96e61703cf
Marcus <marcus.bondezan@accurate.com.br>
2019-07-05 11:24:33 -0300

    Fetching user data and injecting into robots

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import './App.css';
-import { robots } from './robots';
 import CardList from './CardList';
 import SearchBox from './SearchBox';
 
@@ -9,9 +8,15 @@ class App extends Component {
     constructor(){
         super();
         this.state = {
-            robots: robots,
+            robots: [],
             searchfield: ''
         }
+    }
+
+    componentDidMount() {
+        fetch('https://jsonplaceholder.typicode.com/users')
+        .then( response => response.json())
+        .then( users => this.setState({ robots: users }));   
     }
 
     onSearchChange = (event) => {
@@ -24,13 +29,17 @@ class App extends Component {
             return robot.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
         });
 
-        return (
-            <div className='tc'>
-                <h1 className='f1'>RobotFriends</h1>
-                <SearchBox searchChange={this.onSearchChange} />
-                <CardList robots={filteredRobots} />
-            </div>
-        );
+        if(this.state.robots.length === 0){
+            return <h1>Loading... </h1>
+        } else {
+            return (
+                <div className='tc'>
+                    <h1 className='f1'>RobotFriends</h1>
+                    <SearchBox searchChange={this.onSearchChange} />
+                    <CardList robots={filteredRobots} />
+                </div>
+            );
+        }
     }
 }
 
