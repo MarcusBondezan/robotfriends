c051a86cd67baaba4ebaf7d73fb9193c3b4aedfa
Marcus <marcus.bondezan@accurate.com.br>
2019-07-05 16:20:39 -0300

    Using redux to handle searchField state

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,17 +1,30 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { setSearchField } from '../actions';
 import './App.css';
 import ErrorBoundary from '../components/ErrorBoundary';
 import CardList from '../components/CardList';
 import SearchBox from '../components/SearchBox';
 import Scroll from '../components/Scroll';
 
+const mapStateToProps = state => {
+    return {
+        searchField: state.searchField
+    }
+}
+
+const mapDispatchToProps = (dispatch) => {
+    return {
+        onSearchChange: (event) => dispatch(setSearchField(event.target.value))
+    }
+}
+
 class App extends Component {
 
     constructor(){
         super();
         this.state = {
-            robots: [],
-            searchfield: ''
+            robots: []
         }
     }
 
@@ -21,25 +34,22 @@ class App extends Component {
         .then( users => this.setState({ robots: users }));   
     }
 
-    onSearchChange = (event) => {
-        this.setState({ searchfield: event.target.value });
-    }
-
     render(){
-        const { robots, searchfield } = this.state;
+        const { robots } = this.state;
+        const { searchField, onSearchChange } = this.props;
         const filteredRobots = robots.filter(robot => {
-            return robot.name.toLowerCase().includes(searchfield.toLowerCase());
+            return robot.name.toLowerCase().includes(searchField.toLowerCase());
         });
 
-        return robots.length ? this.getPage(filteredRobots) : <h1>Loading... </h1>;
+        return robots.length ? this.getPage(filteredRobots, onSearchChange) : <h1>Loading... </h1>;
         
     }
 
-    getPage(robots){
+    getPage(robots, onSearchChange){
         return (
             <div className='tc'>
                 <h1 className='f1'>RobotFriends</h1>
-                <SearchBox searchChange={this.onSearchChange} />
+                <SearchBox searchChange={onSearchChange} />
                 <Scroll>
                     <ErrorBoundary>
                         <CardList robots={robots} />
@@ -50,4 +60,4 @@ class App extends Component {
     }
 }
 
-export default App;
+export default connect(mapStateToProps, mapDispatchToProps)(App);
